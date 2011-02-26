3e562306d2aef831ab6c22c9dab7abc398136620
jgillick <j_gillick@yahoo.com>
2011-02-26 11:15:07 -0800

    Update comments to the plugin

--- comments.php
+++ comments.php
@@ -1,1 +1,2 @@
-<fb:comments width="560" send_notification_uid="763639132" notify="true"></fb:comments>
+<?php /*<fb:comments width="560" send_notification_uid="763639132" notify="true"></fb:comments>*/ ?>
+<?php if (function_exists('facebook_comments')) facebook_comments(); ?>
